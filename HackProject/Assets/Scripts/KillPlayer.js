5c3b51530f6bf4d4c6739acf9cb44d962f7a0c6a
Felix Hortelano <felix.hortelano@est.fib.upc.edu>
2016-02-20 10:04:12 +0100

    Level1

--- HackProject/Assets/Scripts/KillPlayer.js
+++ HackProject/Assets/Scripts/KillPlayer.js
@@ -11,6 +11,7 @@ function Update () {
 function OnCollisionEnter (col : Collision) {
     //Para debuguear: Debug.Log("1");
     if (col.gameObject.tag == "Player") {
+    	Debug.Log("Collision");
         Application.LoadLevel(Application.loadedLevel);
         //col.gameObject.GetComponent<PlayerDie>().Dead (no se puede porque es privada)
     }
