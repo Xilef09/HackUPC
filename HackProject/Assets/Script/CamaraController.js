8f25aa45880e595f1910be9f5544d8021b7de953
Daniel Farre <daniel.farre@est.fib.upc.edu>
2016-02-20 02:29:40 +0100

    escena2v1

---
+++ HackProject/Assets/Script/CamaraController.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+private var player: Transform;
+
+//private var offset : Vector3;
+
+function Start () {
+	player = GameObject.FindGameObjectWithTag("Player").transform;
+	var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    cube.AddComponent.<Rigidbody>();
+    cube.transform.position = Vector3 (14, 3, -3);
+}
+/*
+function LateUpdate () {
+	transform.position = player.transform.position + offset;
+}*/
+
+function Update () {
+	var aux : Vector3 = 4 * player.up - 3.0 * player.forward;
+	transform.position = player.position + aux;
+	transform.LookAt(player, player.up);
+}
