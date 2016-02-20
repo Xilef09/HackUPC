52d358ae31ff25e264b02e9b3925ea9297c9d198
German Mora <german.eduardo.mora@est.fib.upc.edu>
2016-02-20 04:42:57 +0100

    Escene03

--- HackProject/Assets/Scripts/BarrierUpDown.js
+++ HackProject/Assets/Scripts/BarrierUpDown.js
@@ -1,9 +1,20 @@
 ﻿#pragma strict
 
+public var speed : float = 10f;
+private var goingUp : int = 1;
+
 function Start () {
 
 }
 
 function Update () {
+    //Debug.Log("1");
+    if (transform.position.y >= 10){ 
+        goingUp = -1;
+    }
 
+    else if (transform.position.y <= 1) {
+        goingUp = 1;
+    }
+    transform.position += Vector3.up*speed*Time.deltaTime*goingUp;
 }
