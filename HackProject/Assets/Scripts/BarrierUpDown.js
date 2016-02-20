17b16f7b49d3541f828a3313bd6ca9c70f0277b2
German Mora <german.eduardo.mora@est.fib.upc.edu>
2016-02-20 12:56:07 +0100

    Scene03v7

--- HackProject/Assets/Scripts/BarrierUpDown.js
+++ HackProject/Assets/Scripts/BarrierUpDown.js
@@ -9,12 +9,8 @@ function Start () {
 
 function Update () {
     //Debug.Log("1");
-<<<<<<< HEAD
-    if (transform.position.y >= 230){ 
-=======
-    Debug.Log(transform.position.y);
+
     if (transform.position.y >= 10){ 
->>>>>>> 3370f264803426d642511bcda732d97d804d6175
         goingUp = -1;
     }
 
